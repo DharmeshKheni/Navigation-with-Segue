4773bb815395643d7e657795668146911e9d0370
Dharmesh <khenid21@gmail.com>
2015-05-02 11:50:38 +0530

    navigation with segue
    navigation with segue

---
+++ demo/SecondViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SecondViewController.swift
+//  demo
+//
+//  Created by Anil on 02/05/15.
+//  Copyright (c) 2015 Variya Soft Solutions. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+}
